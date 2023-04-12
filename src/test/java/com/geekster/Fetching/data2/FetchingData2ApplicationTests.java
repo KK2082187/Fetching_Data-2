1641b11c99005b317f5b72355944f36dacdfb076
Komal Kumar <kk2082187@gmail.com>
2023-04-12 15:20:36 +0530

    Fetch the data

---
+++ src/test/java/com/geekster/Fetching/data2/FetchingData2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.Fetching.data2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchingData2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
